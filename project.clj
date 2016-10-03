d4b613dfef7618c9665136432aefab551ca7e0b7
Esteban <eginez@gmail.com>
2016-10-02 20:13:37 -0700

    Adding license

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :url "https://github.com/eginez/huckleberry"
   :description "maven dependecy resolution in clojurescript"
   :min-lein-version "2.5.3"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
