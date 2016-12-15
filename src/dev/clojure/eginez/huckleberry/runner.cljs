179f7a51fea878ae4c9e7067792598fb70302ab1
Andrea Richiardi <a.richiardi.work@gmail.com>
2016-12-15 13:19:29 -0800

    Add runner/main for figwheel's repl
    In the first terminal run `lein figwheel repl`, in the second terminal run `lein run`.

---
+++ src/dev/clojure/eginez/huckleberry/runner.cljs
@@ -0,0 +1,9 @@
+(ns ^:figwheel-always eginez.huckleberry.runner
+  (:require [cljs.nodejs :as nodejs]))
+
+(nodejs/enable-util-print!)
+
+(defn -main []
+  (println "Huckleberry runner started!"))
+
+(set! cljs.core/*main-cli-fn* -main)
